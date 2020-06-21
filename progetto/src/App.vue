2e92db8d5c969c758b4cb4b7f1d6b17867f532ac
rota99 <rota99@users.noreply.github.com>
2020-06-21 18:34:11 +0200

    Inizializzazione

---
+++ progetto/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">Lo progetto</span>
+      </md-app-toolbar>
+      <md-app-content>
+        <router-view></router-view>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
