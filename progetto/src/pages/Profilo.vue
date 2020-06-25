6f64bddcd65c8baa20ce35d3a3eab9f3cb3ae917
rota99 <rota99@users.noreply.github.com>
2020-06-25 20:26:04 +0200

    Inizio stile profilo

--- progetto/src/pages/Profilo.vue
+++ progetto/src/pages/Profilo.vue
@@ -1,4 +1,9 @@
 <template>
+  <div>
+    <md-avatar class="md-large">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg" />
+    </md-avatar>
+  </div>
 </template>
 
 <script>
