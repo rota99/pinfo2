b55a1b573ce800db32bfcc364d0b8a1fd11ff264
rota99 <rota99@users.noreply.github.com>
2020-06-27 16:05:57 +0200

    Aggiunta possibilità di scrivere un post

--- progetto/src/pages/Dashboard.vue
+++ progetto/src/pages/Dashboard.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
+  <div class="md-layout md-alignment-top-center">
+    <div class="md-layout-item md-large-size-66">
+      <md-card>
+        <md-card-header>
+          <md-avatar>
+            <!--struttura dell'immagine <img :src="url" />.
+            l'url va preso dal db, quindi va fatta una funzione getUserInfo() in dataservice che recupera le informazioni
+            dell'utente sapendo il suo username-->
+            <img>
+          </md-avatar>
+          <span class="md-title">{{ username }}</span>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field>
+            <label>Scrivi qualcosa...</label>
+            <md-textarea v-model="postContent" md-autogrow></md-textarea>
+          </md-field>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button class="md-icon-button">
+            <md-icon>send</md-icon>
+          </md-button>
+        </md-card-actions>
+      </md-card>
+    </div>
   </div>
 </template>
 
 <script>
- export default{
-   created:function(){
-     console.log('ciao');
-   }
- }
+export default {
+  data: function() {
+    return {
+      username: localStorage.getItem('username'),
+      postContent: null
+    }
+  }
+}
 </script>
 
 <style>
