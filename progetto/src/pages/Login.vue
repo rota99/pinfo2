2bb86e678c79009c2db65720cbfea6d0a4e0947f
rota99 <rota99@users.noreply.github.com>
2020-06-27 12:15:10 +0200

    Completato il login con paese e immagine del profilo

--- progetto/src/pages/Login.vue
+++ progetto/src/pages/Login.vue
@@ -9,9 +9,19 @@
           <label>Username</label>
           <md-input v-model="username"></md-input>
         </md-field>
+        <md-field>
+          <label>Paese</label>
+          <md-select v-model="paese">
+            <md-option v-for="country in countries" :key="country" value="country">{{ country }}</md-option>
+          </md-select>
+        </md-field>
+        <md-field>
+          <label>Immagine del profilo (link)</label>
+          <md-input v-model="img"></md-input>
+        </md-field>
       </md-card-content>
       <md-card-actions>
-        <md-button class="md-primary md-raised" :disabled="!username" @click="signIn()">Sign in</md-button>
+        <md-button class="md-primary md-raised" :disabled="(!username || !paese || !img)" @click="signIn()">Sign in</md-button>
       </md-card-actions>
     </md-card>
   </div>
@@ -24,23 +34,26 @@ export default {
   data: function() {
     return {
       username: '',
-      countries:[]
+      paese: '',
+      img: '',
+      countries: []
     }
   },
-  created:function(){
+  created: function() {
     this.load();
   },
   methods: {
     signIn: function() {
       DataService.login(this.username);
-      this.$router.push({path: '/'});
+      this.$router.push({ path: '/' });
     },
-    load:function(){
-      DataService.getCountries().then(data =>{
-        for(var i=0;i<data.data.length;i++){
-          this.countries[i]=data.data[i].Country;
+    load: function() {
+      DataService.getCountries().then(data => {
+        for(var i=0; i<data.data.length; i++) {
+          this.countries[i] = data.data[i].Country;
         }
-        console.log(this.countries);
+
+        this.countries.sort();
       });
     }
   }
