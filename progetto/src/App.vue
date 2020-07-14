67e9d76507e8a235703e468ba1c2b4ecaa111d7c
rota99 <rota99@users.noreply.github.com>
2020-07-14 11:33:51 +0200

    Nuovo menu

--- progetto/src/App.vue
+++ progetto/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <md-app>
-      <md-app-toolbar class="md-primary md-small-hide" md-theme="default">
+    <div class="page-container md-layout-column">
+      <md-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="showNavigation = true">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">LockedIn</span>
 
-        <div class="md-toolbar-section-start">
-          <span class="md-title">LockedIn</span>
-        </div>
-        <div class="md-toolbar-section-end md-small-hide">
+        <div class="md-toolbar-section-end">
           <md-tabs class="md-primary" md-sync-route>
             <md-tab id="tab-dashboard" md-label="Dashboard" :to="'/dashboard'"></md-tab>
             <md-tab id="tab-profilo" md-label="Profilo" :to="'/profilo/' + username"></md-tab>
@@ -16,19 +17,58 @@
             <md-icon>exit_to_app</md-icon>
           </md-button>
         </div>
-      </md-app-toolbar>
+      </md-toolbar>
 
+      <md-drawer :md-active.sync="showNavigation" md-swipeable>
+        <md-toolbar class="md-transparent md-large" md-elevation="0">
+          <div class="md-toolbar-row">
+            <div class="md-toolbar-section-start">
+              <md-avatar>
+                <img />
+              </md-avatar>
+            </div>
 
-      <md-app-content>
+            <div class="md-toolbar-section-end">
+              <md-button class="md-icon-button md-dense" @click="showNavigation = false">
+                <md-icon>keyboard_arrow_left</md-icon>
+              </md-button>
+            </div>
+          </div>
+
+          <div class="md-toolbar-row md-toolbar-offset">
+            <h3 class="md-title">{{ username }}</h3>
+          </div>
+        </md-toolbar>
+
+        <md-list md-sync-route>
+          <md-list-item :to="'/dashboard'">
+            <md-icon>dashboard</md-icon>
+            <span class="md-list-item-text">Dashboard</span>
+          </md-list-item>
+
+          <md-list-item :to="'/profilo/' + username">
+            <md-icon>person</md-icon>
+            <span class="md-list-item-text">Profilo</span>
+          </md-list-item>
+
+          <md-list-item :to="'/contagi/' + country">
+            <md-icon>insert_chart</md-icon>
+            <span class="md-list-item-text">Contagi</span>
+          </md-list-item>
+
+          <md-divider></md-divider>
+
+          <md-list-item @click="logout()">
+            <md-icon>exit_to_app</md-icon>
+            <span class="md-list-item-text">Logout</span>
+          </md-list-item>
+        </md-list>
+      </md-drawer>
+
+      <md-content>
         <router-view></router-view>
-        <md-bottom-bar class="md-primary" md-type="shift" md-sync-route>
-          <md-bottom-bar-item :to="'/dashboard'" exact md-label="Dashboard" md-icon="dashboard"></md-bottom-bar-item>
-          <md-bottom-bar-item :to="'/profilo/' + username" md-label="Profilo" md-icon="person"></md-bottom-bar-item>
-          <md-bottom-bar-item :to="'/contagi/' + country" md-label="Contagi" md-icon="insert_chart"></md-bottom-bar-item>
-        </md-bottom-bar>
-      </md-app-content>
-
-    </md-app>
+      </md-content>
+    </div>
   </div>
 </template>
 
@@ -40,7 +80,8 @@ export default {
   data: function() {
     return {
       username: localStorage.getItem('username'),
-      country: localStorage.getItem('country')
+      country: localStorage.getItem('country'),
+      showNavigation: false
     }
   },
   methods: {
