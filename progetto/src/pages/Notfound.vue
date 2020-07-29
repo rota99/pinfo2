de034dd5fa5d020f6f4f231d7095d569964cbe5f
rota99 <rota99@users.noreply.github.com>
2020-07-29 17:24:36 +0200

    Aggiunta pagina di errore 404

---
+++ progetto/src/pages/Notfound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="md-title">404 Pagina non trovata.</div>
+    <md-button :to="'/dashboard'" class="md-primary md-raised">Torna alla bacheca</md-button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- progetto/src/router/index.js
+++ progetto/src/router/index.js
@@ -6,6 +6,7 @@ import Dashboard from '../pages/Dashboard'
 import Profilo from '../pages/Profilo'
 import Contagi from'../pages/Contagi'
 import Osservati from'../pages/Osservati'
+import Notfound from '../pages/Notfound'
 
 Vue.use(Router)
 
@@ -17,6 +18,7 @@ export default new Router({
     {name: 'Profilo', path: '/profilo/:username', component: Profilo},
     {name: 'Contagi', path: '/contagi/:slug', component: Contagi},
     {name: 'Osservati', path: '/osservati', component: Osservati},
-    {name: 'default', path: '/', redirect: '/dashboard'}
+    {name: 'default', path: '/', redirect: '/dashboard'},
+    {name: '404', path: '*', component: Notfound}
   ]
 })
